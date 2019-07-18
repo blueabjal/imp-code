b4ea13ae3cd676fc60c77e54a78e547c1eedb2ad
blueabjal <41937270+blueabjal@users.noreply.github.com>
2019-07-19 04:33:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu
-git init
-git clone https://github.com/blueabjal/imp-code.git
 cd imp-code
 sudo sh -x script.sh >> out.sh
 sudo minikube start --vm-driver=none >> out.sh
