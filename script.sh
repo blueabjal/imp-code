465ca340d6ac21747af270d7bf648108320e4409
blueabjal <41937270+blueabjal@users.noreply.github.com>
2019-07-19 04:30:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt-get update -y &&  apt-get install -y docker.io
-sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-sudo curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-sudo minikube start --vm-driver=none
+cd /home/ubuntu
+git init
+git clone https://github.com/blueabjal/imp-code.git
+cd imp-code
+sudo sh -x script.sh >> out.sh
+sudo minikube start --vm-driver=none >> out.sh
