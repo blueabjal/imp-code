b5a7a95f3ec5bdfa5129552050aa712cc3151a4b
blueabjal <41937270+blueabjal@users.noreply.github.com>
2019-07-19 09:25:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 sudo apt-get update -y &&  apt-get install -y docker.io
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 sudo curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 sudo minikube start --vm-driver=none
+
